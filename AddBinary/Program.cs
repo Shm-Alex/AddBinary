294acfce33afbb6d3c11cfa56a11d0703c09f823
shmykov.alexander <shmykov.alexander@gmail.com>
2022-11-18 10:45:31 +0300

    Add project files.

---
+++ AddBinary/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NAddBinary
+{
+    internal class Program
+    {
+        public class Solution
+        {
+            public string AddBinary(string a, string b)
+            {
+                return a + b;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Solution s = new Solution();
+            Console.WriteLine(s.AddBinary("1110", "1000111"));
+        }
+    }
+}
